79fae663247d8ddfe008fd6008cc1ead0dd67a0a
LucasHarff <lucasharff@hotmail.com>
2020-07-08 22:53:11 -0300

    começando a configurar o scraps

---
+++ bundle.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var TaskList = function TaskList() {
+  _classCallCheck(this, TaskList);
+
+  this.titleInput = document.getElementById("messageTitle");
+  this.messageInput = document.getElementById("messageBody");
+  this.addButton = document.getElementById("addButton");
+  this.scrapsField = document.getElementById("scrapsField");
+  this.scraps = [];
+};
---
+++ main.js
@@ -0,0 +1,10 @@
+class TaskList {
+  constructor() {
+    this.titleInput = document.getElementById("messageTitle");
+    this.messageInput = document.getElementById("messageBody");
+    this.addButton = document.getElementById("addButton");
+    this.scrapsField = document.getElementById("scrapsField");
+
+    this.scraps = [];
+  }
+}
