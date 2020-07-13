286b167ff9673950f590f5750cb76857330f125e
LucasHarff <lucasharff@hotmail.com>
2020-07-13 20:12:05 -0300

    criamos o scrap e o botao de delete

--- main.js
+++ main.js
@@ -7,38 +7,65 @@ class TaskList {
 
     this.scraps = [];
 
-
+    this.registerEvents();
   }
 
-  registerEvents(){
-      this.addButton.onclick = 
+  setAddButtonEvent() {
+    this.addButton.onclick = () => this.addNewScrap();
   }
 
-  renderScraps(){
-      this.scrapsField.innerHTML = ""
+  setButtonEvents() {
+    console.log(document.querySelectorAll(".delete-button"));
+  }
 
-      for(const scrap of this.scraps){
-          this.scrapsField.innerHTML += this.createScrapCard(scrap.title, scrap.message)
-      }
+  renderScraps() {
+    this.scrapsField.innerHTML = "";
+
+    for (const scrap of this.scraps) {
+      let position = scraps.indexOf(scrap);
+      this.scrapsField.innerHTML += this.createScrapCard(
+        scrap.title,
+        scrap.message,
+        position
+      );
+    }
+    this.setButtonEvents();
+  }
+
+  addNewScrap() {
+    let title = this.titleInput.value;
+    let message = this.messageInput.value;
+
+    this.titleInput.value = "";
+    this.messageInput.value = "";
+
+    this.scraps.push({ title, message });
+
+    this.renderScraps();
+  }
+
+  deleteScrap(position) {
+    this.scraps.splice(position, 1);
+
+    this.renderScraps();
+  }
+
+  createScrapCard(title, message) {
+    return `
+      <div class="message-cards card text-white bg-dark m-2 col-3">
+        <div class="card-header font-weight-bold">${title}</div>
+        <div class="card-body">
+          <p class="card-text">
+            ${message}
+          </p>
+        </div>
+        <div class="w-100 d-flex justify-content-end pr-2 pb-2">
+          <button class="btn btn-danger mr-1 delete-button">Deletar</button>
+          <button class="btn btn-info">Editar</button>
+        </div>
+      </div>
+    `;
   }
 }
 
-createScrapCard(title, message){
-    return
-    <div class="message-cards card text-white bg-dark m-2">
-  <div class="card-header font-weight-bold">${title}</div>
-  <div class="card-body">
-    <p class="card-text">
-      ${message}
-    </p>
-  </div>
-  <div class="w100 d-flex justify-content-end pr-2 pb-2">
-    <button class="btn btn-danger mr-1" onclick="deleteCards(${position})" >Excluir</button>
-    <button class="btn btn-info"
-    onclick="openEditModal(${position})"
-    >
-    Editar
-    </button>
-  </div>
-</div>
-}
+new TaskList();
